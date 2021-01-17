5af777aefe217e35f01e810a365397dfa11ea6a2
RushikeshNikumbh <RushikeshNikumbh@github.com>
2021-01-17 23:52:40 +0530

    commit on 'master'

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
-		document.querySelector('#myBtn1')
+	/*	document.querySelector('#myBtn1')
 		.addEventListener('click',() => {
 			window.open( 
               "Get Involved.html", "_blank");
@@ -8,6 +8,22 @@
 		.addEventListener('click',() => {
 			window.open( 
               "rcpve.com", "_blank");
-        });		
+        });	*/	
         
-        
+        function joinRotary() {
+            window.open('https://my.rotary.org/en', "_blank")
+          }
+
+
+          function joinRotaract() {
+            window.open('https://www.rotary.org/en/get-involved/rotaract-clubs', "_blank")
+          }
+
+
+          function getStarted() {
+            window.open('Get Involved.html', "_blank")
+          }
+
+          function makeDonation(){
+                window.open('Get Involved.html', "_blank")
+          }
