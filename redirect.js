84cdbc012db9db203fed791a52f26239793ad275
RushikeshNikumbh <RushikeshNikumbh@github.com>
2021-01-17 20:04:13 +0530

    Update version 1.1

---
+++ redirect.js
@@ -0,0 +1,13 @@
+		document.querySelector('#myBtn1')
+		.addEventListener('click',() => {
+			window.open( 
+              "Get Involved.html", "_blank");
+        });		
+        
+        document.querySelector('#myBtn')
+		.addEventListener('click',() => {
+			window.open( 
+              "rcpve.com", "_blank");
+        });		
+        
+        
